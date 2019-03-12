0292fbb64dbd9d849ea8e33788b2b6e8c0af2961
Henry McCreery <henrymccy@gmail.com>
2019-03-12 14:09:13 -0700

    Created magic_test.py and tried slicing
    It works really well

---
+++ magic_test.py
@@ -0,0 +1,5 @@
+x = "jknv qwdklb alkwjef aljfln sdfnklsjnal aglwkgn asdfkjbadjhbjw ads alkjsfba ad kljabfk afhbawblkf afhblsbdaflkjk awlkjfbklwabfkjln awfkljbwablk"
+y = "jknv qwdulb alkwjef aljfln sdfnklsjnal aglwkgm asdfkjbadjhbjw ads alkjsfba ad kljabfk afhbawblkf afhblsbdajlkjk awlkjfbklwabfkjln awfoojbwablk"
+x_segment = x[50:100]
+y_segment = y[50:100]
+# print(len(x_segment))
