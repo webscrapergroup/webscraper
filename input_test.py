2e0fbc06c525be2878bdfa8b4167d314966f11c5
Zoonn123 <43764940+Zoonn123@users.noreply.github.com>
2019-03-12 14:17:03 -0700

    added imports

--- input_test.py
+++ input_test.py
@@ -1,3 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+from twilio.rest import Client
+import time
+import argparse
+import html2text
+
+
 x = "jknv qwdklb alkwjef aljfln sdfnklsjnal aglwkgn asdfkjbadjhbjw ads alkjsfba ad kljabfk afhbawblkf afhblsbdaflkjk awlkjfbklwabfkjln awfkljbwablk"
 y = "jknv qwdulb alkwjef aljfln sdfnklsjnal aglwkgm asdfkjbadjhbjw ads alkjsfba ad kljabfk afhbawblkf afhblsbdajlkjk awlkjfbklwabfkjln awfoojbwablk"
 x_segment = x[50:100]
