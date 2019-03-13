e6d080eda70cdc9201b1e74b7e88e41d2b6da442
Henry McCreery <henrymccy@gmail.com>
2019-03-13 13:29:33 -0700

    minor updates & changes

--- magic_test.py
+++ magic_test.py
@@ -1,8 +1,13 @@
 x = "jknv qwdklb alkwjef aljfln sdfnklsjnal aglwkgn asdfkjbadjhbjw ads alkjsfba ad kljabfk afhbawblkf afhblsbdaflkjk awlkjfbklwabfkjln awfkljbwablk"
-y = "jknv qwdulb alkwjef aljfln sdfnklsjnal aglwkgm asdfkjbadjhbjw ads alkjsfba ad kljabfk afhbawbokf afiblsbdajlkjk awlkjfbklwabfkjln awfoojbwablk"
+y = "jknv qwdulb alkwjef aljfln sdfnklsjnal mglwkgm asdfkjtadjhbjw ads alkjsfba ad kljabmk afhbawbokf afiblsbdajlnjk nwlkjfbklwabfkjln awfoojbwablk"
 x_segment = x[50:100]
 y_segment = y[50:100]
+# print(x)
+# print(len(x)) #142
+# print(x_segment)
+# print(len(x_segment)) #50
 # print(len(x_segment))
+
 # from difflib import Differ
 # import re
 # # for (i, j) in (x_segment, y_segment):
@@ -22,4 +27,4 @@ y_segment = y[50:100]
 # # print(regex_test)
 
 count = sum(1 for a, b in zip(x_segment, y_segment) if a != b)
-print(count)
+print(str(count) + " changes")
