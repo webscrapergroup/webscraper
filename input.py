8f2e71b82622005b8b7eed1433e701b7da1152c8
Henry McCreery <henrymccy@gmail.com>
2019-03-15 08:42:38 -0700

    Added basic loop in magic.py and details in input.py

--- input.py
+++ input.py
@@ -1,3 +1,9 @@
 
-text_old = ""
-text_new = ""
+# Things needed by magic.py:
+# 1. Functions:
+#     1. get_page_contents
+# 2. variables:
+#     1. user_interval
+#     2. user_max_checks
+#     3. user_tolerance
+#     4. user_end_after_success
--- magic.py
+++ magic.py
@@ -1,4 +1,23 @@
 
-from input import get_page_contents
-count = sum(1 for a, b in zip(text_old, text_new) if a != b)
+import time
+from input import get_page_contents, user_interval, user_max_checks, user_tolerance, user_end_after_success
+# count = sum(1 for a, b in zip(text_old, text_new) if a != b)
 
+send_text = False
+check_count = 0
+while True:
+    contents_old = get_page_contents()
+    time.sleep(user_interval)
+    contents_new = get_page_contents()
+    differences_count = sum(1 for a, b in zip(contents_old, contents_new) if a != b)
+    if(differences_count >= user_tolerance):
+        send_text = True
+        if(check_count >= user_max_checks or user_end_after_success == True):
+            break
+        else:
+            continue
+    else:
+        if(check_count >= user_max_checks):
+            break
+        else:
+            continue
