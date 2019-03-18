8630c88c5139ed1595876633fac768271c14e7e9
Orion Capell <orion.capell@gmail.com>
2019-03-18 13:59:37 -0700

    Added variables needed for output.py to work

--- input.py
+++ input.py
@@ -7,3 +7,9 @@
 #     2. user_max_checks
 #     3. user_tolerance
 #     4. user_end_after_success
+# Things needed by output.py:
+# 1. Variables:
+#     1. twilio_num
+#     2. client_num
+#     3. account_sid
+#     4. auth_token
