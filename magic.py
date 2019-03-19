8fe8255f6d1a77137f36dcce66cfeb78f499467a
Corinne Hare <43765004+Corinnehuilin@users.noreply.github.com>
2019-03-19 11:04:22 -0700

    increment check count

--- magic.py
+++ magic.py
@@ -1,22 +1,20 @@
 import time
 from input import get_page_contents, user_interval, user_max_checks, user_tolerance, user_end_after_success
-# count = sum(1 for a, b in zip(text_old, text_new) if a != b)
 
 send_text = False
 check_count = 0
-while True:
-    contents_old = get_page_contents()
+
+contents_old = get_page_contents()
+
+while check_count < user_max_checks:
     time.sleep(user_interval)
     contents_new = get_page_contents()
+    check_count += 1
+    
     differences_count = sum(1 for a, b in zip(contents_old, contents_new) if a != b)
     if(differences_count >= user_tolerance):
         send_text = True
-        if(check_count >= user_max_checks or user_end_after_success == True):
+        if(user_end_after_success == True):
             break
-        else:
-            continue
-    else:
-        if(check_count >= user_max_checks):
-            break
-        else:
-            continue
+            
+    contents_old = contents_new
