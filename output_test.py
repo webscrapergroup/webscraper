e5c72754ec6afdb3acd8757c340220f5b171d916
Orion Capell <orion.capell@gmail.com>
2019-04-22 11:46:03 -0700

    Added more code to the output

--- output_test.py
+++ output_test.py
@@ -14,22 +14,40 @@ from input import (
     user_phone,
 )
 
+# Strings for text messages and terminal messages
+change_detected = "We have detected a change!"
+change_not_detected = "We did not detect a change."
+
 
 def found_change():
     if send_text == True:
-        # Message for if change detected
-        client.message.create(
-            body="We have detected a change!", from_=twilio_phone, to=user_phone
-        )
-        # if(user_end_after_success == True):
-        #     break
-        # else:
-        #     time.sleep(user_interval)
+        # Message to the client's phone and terminal for if change detected
+        client.message.create(body=change_detected, from_=twilio_phone, to=user_phone)
+        print(change_detected)
+        # If the user decides to have the program continue running, program sleeps and then runs again
+        if user_end_after_success == False:
+            time.sleep(user_interval)
+            found_change()
+        else:
+            continue
+        # Only terminal message for if not text message
+    else:
+        print(change_detected)
 
 
 def no_change():
     if send_text == True:
-        # Message for if change not detected
+        # Message to the client's phone and terminal for if change not detected
         client.message.create(
-            body="We did not detect a change.", from_=twilio_phone, to=user_phone
+            body=change_not_detected, from_=twilio_phone, to=user_phone
         )
+        print(change_not_detected)
+        # If the user decides to have the program continue running, program sleeps and then runs again
+        if user_end_after_success == False:
+            time.sleep(user_interval)
+            found_change()
+        else:
+            continue
+        # Only terminal message instead of text message
+    else:
+        print(change_not_detected)
