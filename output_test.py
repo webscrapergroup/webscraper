a7d10fc734eff81633247455f779345e59725dc7
Orion Capell <orion.capell@gmail.com>
2019-05-25 15:00:25 -0700

    Added date and time to message, other fixes

--- output_test.py
+++ output_test.py
@@ -3,25 +3,30 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import argparse
+import datetime
 
 from magic import send_text
 
 # specific variable names still needed?
 from input import (
     client,
-    user_end_after_success,
-    user_max_checks,
-    user_duration,
+    # user_end_after_success,
+    # user_max_checks,
+    # user_duration,
     user_interval,
     twilio_phone,
     user_phone,
 )
 
+# Getting time  and date for when change is detected
+time_checked = datetime.datetime.now().time()
+date_checked = datetime.datetime.now().date()
+
 # String for text messages and terminal messages
-change_detected = "We have detected a change!"
+change_detected = "We have detected a change at " + time_checked + date_checked + "!z"
 
 # The number of successful checks so far
-user_num_checks = 0
+# user_num_checks = 0
 
 
 def found_change():
@@ -30,16 +35,16 @@ def found_change():
         client.message.create(body=change_detected, from_=twilio_phone, to=user_phone)
         print(change_detected)
         # If the user decides to have the program continue running, program sleeps and then runs again
-        if user_end_after_success == False:
-            user_num_checks += 1
-            # Checks if program has reached the maximum checks
-            if user_num_checks != user_max_checks:
-                time.sleep(user_interval)
-                found_change()
-            else:
-                pass
-        else:
-            pass
+        # if user_end_after_success == False:
+        #     user_num_checks += 1
+        #     # Checks if program has reached the maximum checks
+        #     if user_num_checks != user_max_checks:
+        #         time.sleep(user_interval)
+        #         found_change()
+        #     else:
+        #         pass
+        # else:
+        #     pass
         # Only terminal message for if not text message
     else:
         print(change_detected)
