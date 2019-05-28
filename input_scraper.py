fb74ba23d74f4c8949ff1b725886ef03726f62a6
Cheese810 <43764957+Cheese810@users.noreply.github.com>
2019-05-28 14:12:41 -0700

    Update input_scraper.py

--- input_scraper.py
+++ input_scraper.py
@@ -31,6 +31,34 @@ else:
                 user_end_after_success = false
 
 client = Client(account_sid, auth_token)
+               quote_page = https://github.com/webscrapergroup/webscraper
+<li class="commits">
+          <a data-pjax="" href="/webscrapergroup/webscraper/commits/master">
+              <svg class="octicon octicon-history" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 13H6V6h5v2H8v5zM7 1C4.81 1 2.87 2.02 1.59 3.59L0 2v4h4L2.5 4.5C3.55 3.17 5.17 2.3 7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-.34.03-.67.09-1H.08C.03 7.33 0 7.66 0 8c0 3.86 3.14 7 7 7s7-3.14 7-7-3.14-7-7-7z"></path></svg>
+              <span class="num text-emphasized">
+                18
+              </span>
+              commits
+          </a>
+        </li>
+      
+       <span class="num text-emphasized">
+                47
+              </span>         
+       r = requests.get(url)
+       print r.status_code
+       print r.text
+       page = urllib2.urlopen(quote_page)
+       name_box = soup.find('h1', attrs={'class': 'name'})
+       name = name_box.text.strip() 
+                print name
+       
+def tag_from_html(body):
+    soup = BeautifulSoup(page.content, 'html.parser')
+    inner_text = soup.find("div", id="commits").content
+    page.status_code = 200
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
 
 # Things needed by magic.py:
 # 1. Functions:
